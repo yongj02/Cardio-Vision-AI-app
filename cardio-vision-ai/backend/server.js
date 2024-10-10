01b7def236c7450c08d6838b44900b0d9c709d39
Xiao Zheng <xzhe0014@student.monash.edu>
2024-10-10 22:39:56 +1100

    Update CORS origin pathway

--- cardio-vision-ai/backend/server.js
+++ cardio-vision-ai/backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: 'fit4701-4702-fyp-production.up.railway.app:3000', // Allow requests from this origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
   credentials: true, // Allow credentials (if needed)
 };
