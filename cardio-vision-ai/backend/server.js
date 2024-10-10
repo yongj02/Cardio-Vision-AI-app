f9c08538c13cef100b6d63ba92ed182836b0275e
yche0323 <yche0323@student.monash.edu>
2024-10-11 02:07:27 +1100

    using port 8080

--- cardio-vision-ai/backend/server.js
+++ cardio-vision-ai/backend/server.js
@@ -12,7 +12,6 @@ connectDB();
 
 const app = express();
 
-/*
 // CORS configuration
 const corsOptions = {
   origin: 'https://fit4701-4702-fyp-production.up.railway.app', // Allow requests from this origin
@@ -20,22 +19,7 @@ const corsOptions = {
   credentials: true, // Allow credentials (if needed)
 };
 
-app.use(cors(corsOptions)); 
-*/
-
-// Using custom CORS configuration
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://fit4701-4702-fyp-production.up.railway.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -45,7 +29,7 @@ app.use('/api/patients', patientRoutes);
 app.use('/api/datasets', datasetRoutes);
 app.use('/api/predict', predictRoutes);
 
-const PORT = parseInt(process.env.PORT || "8000", 10);
+const PORT = parseInt(process.env.PORT || "8080", 10);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
